14aa859864e5b7986a72c5e38eda0138deda784f
Gustavo Brasil <gubrasil@gmail.com>
2021-08-19 18:57:31 -0300

    login com firebase e cria cadastro

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,34 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Menu from '../../Components/menu';
 import './login.css'
 
-function Login() {
-  return <div className="d-flex align-items-center text-center form-div" >
-    
-      <Menu />
-      <div className="container-fluid">
-      
-      <form className="form-login">
-        <h1 className="h3 mb-3 fw-normal">Login</h1>
+import firebase from '../../config/firebase.js';
+import 'firebase/auth'
 
-        <div className="form-floating">
-          <input type="email" className="form-control" id="floatingInput" placeholder="E-mail" />
-          <label for="floatingInput">E-mail</label>
-        </div>
-        <div className="form-floating">
-          <input type="password" className="form-control" id="floatingPassword" placeholder="Senha" />
-          <label for="floatingPassword">Senha</label>
-        </div>
-        <button className="w-100 btn btn-lg btn-primary" type="button">Acessar</button>
-        <div className="login-links mt-1">
-          <Link to="/" className="mx-3"> Esqueci minha senha</Link>
-          <Link to="/novocadastro" className="mx-3"> Criar Cadastro</Link>
-        </div>
-        <p className="mt-5 mb-3 text-muted">&copy; Desenvolvido por Gustavo Brasil</p>
-      </form>
-    </div>
-  </div>;
+function Login() {
+
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [sucesso, setSucesso] = useState('');
+
+  function LoginUsuario() {
+    firebase.auth().signInWithEmailAndPassword(email, senha)
+      .then(function (result) {
+        setSucesso('S');
+      })
+      .catch(function (error) {
+        setSucesso('N');
+      })
+
+  }
+
+  return <div className="d-flex align-items-center text-center form-container" >
+    <Menu />
+    <form className="form-login">
+      <h1 className="h3 mb-3 fw-normal">Login</h1>
+
+      <div className="form-floating">
+        <input onChange={(event) => setEmail(event.target.value)} type="email" className="form-control" id="floatingInput" placeholder="E-mail" />
+        <label for="floatingInput">E-mail</label>
+      </div>
+      <div className="form-floating">
+        <input onChange={(event) => setSenha(event.target.value)} type="password" className="form-control" id="floatingPassword" placeholder="Senha" />
+        <label for="floatingPassword">Senha</label>
+      </div>
+      <button onClick={LoginUsuario} className="w-100 btn btn-lg btn-primary" type="button">Acessar</button>
+
+      {
+        //verificar se autenticação foi sucesso e emite mensagem de erro
+        sucesso === 'N' ? <div class="alert alert-danger mt-2" role="alert" >E-mail ou senha inválido.</div>: null
+      }
+      
+
+      <div className="login-links mt-1">
+        <Link to="/" className="mx-3"> Esqueci minha senha</Link>
+        <Link to="/novocadastro" className="mx-3"> Criar Cadastro</Link>
+      </div>
+      <p className="mt-5 mb-3 text-muted">&copy; Desenvolvido por Gustavo Brasil</p>
+    </form>
+  </div >;
 }
 
 export default Login;
--- src/pages/cadastro/novoCadastro.jsx
+++ src/pages/cadastro/novoCadastro.jsx
@@ -1,76 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Menu from '../../Components/menu';
 
 import './novoCadastro.css';
 
+import firebase from '../../config/firebase';
+import 'firebase/auth';
+
+
 function NovoCadastro() {
+
+    const [cpf, setCpf] = useState('');
+    const [email, setEmail] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [senha, setSenha] = useState('');
+    const [nome, setNome] = useState('');
+
+
+    function cadastraUsuario() {
+        firebase.adm.auth().createUser({
+            uid: { cpf },
+            email: { email },
+            emailVerified: false,
+            phoneNumber: { telefone },
+            password: { senha },
+            displayName: { nome },
+            photoURL: ' ',
+            disabled: false,
+        })
+            .then((userRecord) => {
+                // See the UserRecord reference doc for the contents of userRecord.
+                console.log('Successfully created new user:', userRecord.uid);
+            })
+            .catch((error) => {
+                console.log('Error creating new user:', error);
+            });
+
+    }
+
     return <div classNameName="d-flex align-items-center text-center form-cad" >
         <Menu />
         <div className="container-fluid">
             <form className="needs-validation" novalidate >
                 <div className="form-row">
-                    <div className="col-lg-4 mb-3">
-                        <label for="validationCustom01">Primeiro nome</label>
-                        <input type="text" className="form-control" id="validationCustom01" placeholder="Nome" required />
-                        <div className="valid-feedback">
-                            Tudo certo!
-                        </div>
-                    </div>
-                    <div className="col-lg-4 mb-3">
-                        <label for="validationCustom02">Sobrenome</label>
-                        <input type="text" className="form-control" id="validationCustom02" placeholder="Sobrenome" required />
-                        <div className="valid-feedback">
-                            Tudo certo!
-                        </div>
-                    </div>
 
-                    <div className="col-lg-4 mb-3">
+                    <div className="col-lg-6 mb-3">
                         <label className="control-label" for="cpf_form">CPF</label>
-                        <input type="text" className="form-control" id="cpf_form" placeholder="CPF" maxlength="14" onkeypress={"formatar('###.###.###-##', this);"} required />
-                        <div className="valid-feedback">
-                            Tudo certo!
-                        </div>
+                        <input onChange={(event) => setCpf(event.target.value)} type="text" className="form-control" id="cpf_form" placeholder="CPF" maxlength="14" onkeypress={"formatar('###.###.###-##', this);"} required />
                     </div>
 
+
+                    <div className="col-lg-6 mb-3">
+                        <label for="validationCustom01">Nome Usuário</label>
+                        <input onChange={(event) => setNome(event.target.value)} type="text" className="form-control" id="validationCustom01" placeholder="Nome" required />
+                                            </div>
+
+
+
+                    <div className="form-row">
+                        <div className="col-md-4 mb-3">
+                            <label for="floatingInput">Email</label>
+                            <input onChange={(event) => setEmail(event.target.value)} type="email" className="form-control" id="floatingInput" placeholder="E-mail" />
+                        </div>
+
+                        <div className="col-md-4 mb-3">
+                            <label for="validationCustom01">Telefone</label>
+                            <input onChange={(event) => setTelefone(event.target.value)} type="text" className="form-control" id="validationCustom01" placeholder="Telefone" required />
+                            </div>
+
+
+                        <div className="col-md-4 mb-3">
+                            <label for="floatingPassword">Senha</label>
+                            <input onChange={(event) => setSenha(event.target.value)} type="password" className="form-control" id="floatingPassword" placeholder="Senha" />
+                        </div>
+
+                        <button onClick={cadastraUsuario} className="btn btn-primary" type="button">Enviar</button>
+                    </div>
+                    
                 </div>
-                <div className="form-row">
-                    <div className="col-md-6 mb-3">
-                        <label for="validationCustom03">Cidade</label>
-                        <input type="text" className="form-control" id="validationCustom03" placeholder="Cidade" required />
-                        <div className="invalid-feedback">
-                            Por favor, informe uma cidade válida.
-                        </div>
-                    </div>
-                    <div className="col-md-3 mb-3">
-                        <label for="validationCustom04">Estado</label>
-                        <input type="text" className="form-control" id="validationCustom04" placeholder="Estado" required />
-                        <div className="invalid-feedback">
-                            Por favor, informe um estado válido.
-                        </div>
-                    </div>
-                    <div className="col-md-3 mb-3">
-                        <label for="validationCustom05">CEP</label>
-                        <input type="text" className="form-control" id="validationCustom05" placeholder="CEP" required />
-                        <div className="invalid-feedback">
-                            Por favor, informe um CEP válido.
-                        </div>
-                    </div>
-                </div>
-                <div className="form-group">
-                    <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="invalidCheck" required />
-                        <label className="form-check-label" for="invalidCheck">
-                            Concordo com os termos e condições
-                        </label>
-                        <div className="invalid-feedback">
-                            Você deve concordar, antes de continuar.
-                        </div>
-                    </div>
-                </div>
-                <button className="btn btn-primary" type="submit">Enviar</button>
+
             </form>
+
         </div>
     </div>;
 }
-
 export default NovoCadastro;
