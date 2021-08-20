1595e3cb11da55d6a1f181700c30e09d64e8e581
Gustavo Brasil <gubrasil@gmail.com>
2021-08-20 17:43:15 -0300

    alterações nos icones de cadastro

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBHo7a0PlunQCBDpl0D6cT-46vinNlCtTM",
-    authDomain: "cadastrovendedorasbarreto.firebaseapp.com",
-    projectId: "cadastrovendedorasbarreto",
-    storageBucket: "cadastrovendedorasbarreto.appspot.com",
-    messagingSenderId: "209860339205",
-    appId: "1:209860339205:web:69e73871ad474a42d01e6c"
+  apiKey: "AIzaSyBHo7a0PlunQCBDpl0D6cT-46vinNlCtTM",
+  authDomain: "cadastrovendedorasbarreto.firebaseapp.com",
+  databaseURL: "https://cadastrovendedorasbarreto-default-rtdb.firebaseio.com",
+  projectId: "cadastrovendedorasbarreto",
+  storageBucket: "cadastrovendedorasbarreto.appspot.com",
+  messagingSenderId: "209860339205",
+  appId: "1:209860339205:web:12b523d579f12b33d01e6c"
   };
 
   // Initialize Firebase
--- src/pages/cadastro/novoCadastro.jsx
+++ src/pages/cadastro/novoCadastro.jsx
@@ -15,15 +15,14 @@ function NovoCadastro() {
     const [senha, setSenha] = useState('');
     const [nome, setNome] = useState('');
 
-
     function cadastraUsuario() {
-        firebase.adm.auth().createUser({
-            uid: { cpf },
-            email: { email },
+        firebase.auth().createUser({
+            uid: cpf,
+            email: email,
             emailVerified: false,
-            phoneNumber: { telefone },
-            password: { senha },
-            displayName: { nome },
+            phoneNumber: telefone,
+            password: senha,
+            displayName: nome,
             photoURL: ' ',
             disabled: false,
         })
@@ -40,47 +39,63 @@ function NovoCadastro() {
     return <div classNameName="d-flex align-items-center text-center form-cad" >
         <Menu />
         <div className="container-fluid">
-            <form className="needs-validation" novalidate >
-                <div className="form-row">
-
-                    <div className="col-lg-6 mb-3">
-                        <label className="control-label" for="cpf_form">CPF</label>
+            <form autoComplete="off">
+                <div className="form-row row-1">
+                    <div className="form-group input-group col-md-6" >
+                        <div className="input-group-prepend">
+                            <div className="input-group-text">
+                                <i className="fas fa-user"></i>
+                            </div>
+                        </div>
+                        <input onChange={(event) => setNome(event.target.value)} type="text" className="form-control" placeholder="Nome" name="Nome Completo" />
+                    </div>
+                    <div className="form-group input-group col-md-6" >
+                        <div className="input-group-prepend">
+                            <div className="input-group-text">
+                                <i className="fas fa-user"></i>
+                            </div>
+                        </div>
                         <input onChange={(event) => setCpf(event.target.value)} type="text" className="form-control" id="cpf_form" placeholder="CPF" maxlength="14" onkeypress={"formatar('###.###.###-##', this);"} required />
                     </div>
-
-
-                    <div className="col-lg-6 mb-3">
-                        <label for="validationCustom01">Nome Usuário</label>
-                        <input onChange={(event) => setNome(event.target.value)} type="text" className="form-control" id="validationCustom01" placeholder="Nome" required />
-                                            </div>
-
-
-
-                    <div className="form-row">
-                        <div className="col-md-4 mb-3">
-                            <label for="floatingInput">Email</label>
-                            <input onChange={(event) => setEmail(event.target.value)} type="email" className="form-control" id="floatingInput" placeholder="E-mail" />
-                        </div>
-
-                        <div className="col-md-4 mb-3">
-                            <label for="validationCustom01">Telefone</label>
-                            <input onChange={(event) => setTelefone(event.target.value)} type="text" className="form-control" id="validationCustom01" placeholder="Telefone" required />
-                            </div>
-
-
-                        <div className="col-md-4 mb-3">
-                            <label for="floatingPassword">Senha</label>
-                            <input onChange={(event) => setSenha(event.target.value)} type="password" className="form-control" id="floatingPassword" placeholder="Senha" />
-                        </div>
-
-                        <button onClick={cadastraUsuario} className="btn btn-primary" type="button">Enviar</button>
-                    </div>
-                    
                 </div>
 
-            </form>
+                <div className="form-row row-2">
+                    <div className="form-group input-group col-md-6" >
+                        <div className="input-group-prepend">
+                            <div className="input-group-text">
+                                <i className="fas fa-envelope"></i>
+                            </div>
+                        </div>
+                        <input onChange={(event) => setEmail(event.target.value)} type="email" className="form-control" placeholder="Email" name="Email" />
+                    </div>
 
-        </div>
-    </div>;
+                    <div className="form-group input-group col-md-6" >
+                        <div className="input-group-prepend">
+                            <div className="input-group-text">
+                                <i className="fas fa-mobile"></i>
+                            </div>
+                        </div>
+                        <input onChange={(event) => setTelefone(event.target.value)} type="mobile" className="form-control" placeholder="Telefone" name="Telefone" />
+                    </div>
+
+
+                    <div className="form-group input-group col-md-6" >
+                        <div className="input-group-prepend">
+                            <div className="input-group-text">
+                                <i className="fas fa-lock"></i>
+                            </div>
+                        </div>
+                        <input onChange={(event) => setSenha(event.target.value)} type="password" className="form-control" placeholder="Senha" name="Senha" />
+                    </div>
+                </div>
+
+                <button onClick={cadastraUsuario} className="btn btn-primary" type="button">Enviar</button>
+
+
+
+            </form >
+
+        </div >
+    </div >;
 }
 export default NovoCadastro;
